f7f9ea2d3489e4697b5d508dcd0e124eae92ac3d
honoka5 <s2370179@s.do-johodai.ac.jp>
2024-08-19 06:10:45 +0900

    ブランチ分けました

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,38 @@ import { useState } from 'react'
 
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
   console.log(count)
   return (
-    <>
-      <h1>Hello Would</h1>
-      <div>count:{count}</div>
+    <div>
+      {/*<div>count:{count}</div> ここをコメントアウトしなければcount二個になる*/}
       <CountPreview hoge={count}/>
-      <button onClick={function(){
-        setCount(count+1)
-        console.log(count)
-      }}>+</button>
+      <div id='jo'>
+      <div id='tarako'>
+        <button onClick={function(){
+          setCount(count+1)
+          console.log(count)
+        }}>+</button>
+        <h2 id='me'>増えていくの？</h2>
+      </div>
 
-      <button onClick={function(){
-        setCount(count-1)
-        console.log(count)
-      }}>-</button>
-    </>
+      <div id='goma'>
+        <button onClick={function(){
+          if(count===0){
+            return
+          }
+          setCount(count-1)
+          console.log(count)
+        }}>-</button>
+        <h2>減っていくかも</h2>
+      </div>
+      </div>
+    </div>
   )
 }
 const CountPreview =function({hoge}:{hoge:number}){
-  return <div>count:{hoge}</div>
+  return <div id='jozi'>count:{hoge}</div>
 }
 export default App
